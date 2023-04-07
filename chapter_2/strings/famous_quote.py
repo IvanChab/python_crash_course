741d1f613a3383572be6297f061f3d19c1d80ccd
Wyb <elbrodeahre@gmail.com>
2023-04-07 14:56:46 -0300

    Correct famous_quote.py by using f-strings

--- chapter_2/strings/famous_quote.py
+++ chapter_2/strings/famous_quote.py
@@ -1,3 +1,5 @@
 autor = "Brandon Sanderson"
 phrase = "“The purpose of a storyteller is not to tell you how to think,\n but to give you questions to think upon.”"
-print(phrase+"\n"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+" By "+autor)
+# print(phrase+"\n"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+" By "+autor)
+space = "\t" * 8
+print(f"{phrase}\n{space} By {autor}")
