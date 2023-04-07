73c6add210b57227bc58dfb618c484f3b121d813
Wyb <elbrodeahre@gmail.com>
2023-04-07 14:30:12 -0300

    Add the use of removeprefix

--- chapter_2/strings/file_extensions.py
+++ chapter_2/strings/file_extensions.py
@@ -1,3 +1,8 @@
 filename = 'python_notes.txt'
 print(filename.removesuffix(".txt"))
 # Elimina el ".txt"
+
+url = "https://cualquierweb.com"
+url.removeprefix("https://")
+cualquierweb = url.removeprefix("https://")
+print(cualquierweb)
