84f3d612097c2d9af3972bcba306743bb885f798
Wyb <elbrodeahre@gmail.com>
2023-04-07 15:05:50 -0300

    Add proper use of strip, lstrip and rstrip.

--- chapter_2/strings/stripping_names.py
+++ chapter_2/strings/stripping_names.py
@@ -1,2 +1,10 @@
 name = " \n \t Waldo \t \n "
-print(name.strip().rstrip().lstrip())
+# print(name.strip().rstrip().lstrip())
+variable_1 = name.strip()
+print(variable_1)
+
+variable_2 = name.rstrip()
+print(variable_2)
+
+variable_3 = name.lstrip()
+print(variable_3)
